84ba5c302e6e61ebcf3e4ee0935ee286500882be
Yashwanth1124 <120181936+Yashwanth1124@users.noreply.github.com>
2024-08-25 13:56:37 +0530

    Delete conversely-assingment directory

--- conversely-assingment/Realtime-stock-app/src/components/SearchBar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SearchBar = () => (
-  <div className="search-bar">
-    <input type="text" placeholder="Search for stocks..." />
-  </div>
-)
-
-export default SearchBar
